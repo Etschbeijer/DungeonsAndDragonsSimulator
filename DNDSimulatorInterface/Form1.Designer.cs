f81716a9c7a7fabbb87cf9d3b659a6c7a76c6b6d
patrick blume <PatrickBlume@gmx.de>
2019-10-31 13:28:44 +0100

    Add new gui elements

--- DNDSimulatorInterface/Form1.Designer.cs
+++ DNDSimulatorInterface/Form1.Designer.cs
@@ -38,11 +38,20 @@ namespace DNDSimulatorInterface
             this.charakterCreateButtonLabel = new System.Windows.Forms.Label();
             this.charakterNameTextBox = new System.Windows.Forms.TextBox();
             this.onlyNumberTextBox = new System.Windows.Forms.TextBox();
+            this.CharakterChooseBox = new System.Windows.Forms.ComboBox();
+            this.ActionChooseBox = new System.Windows.Forms.ComboBox();
+            this.CharakterList = new System.Windows.Forms.ListBox();
+            this.CharakterListButton = new System.Windows.Forms.Button();
+            this.CharakterSheet = new System.Windows.Forms.ListBox();
+            this.ShowColumnsButton = new System.Windows.Forms.Button();
+            this.CheckedActionList = new System.Windows.Forms.CheckedListBox();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.UnCheckButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // charakterCreateButton
             // 
-            this.charakterCreateButton.Location = new System.Drawing.Point(293, 185);
+            this.charakterCreateButton.Location = new System.Drawing.Point(279, 167);
             this.charakterCreateButton.Name = "charakterCreateButton";
             this.charakterCreateButton.Size = new System.Drawing.Size(150, 30);
             this.charakterCreateButton.TabIndex = 0;
@@ -52,7 +61,7 @@ namespace DNDSimulatorInterface
             // charakterCreateButtonLabel
             // 
             this.charakterCreateButtonLabel.AutoSize = true;
-            this.charakterCreateButtonLabel.Location = new System.Drawing.Point(345, 218);
+            this.charakterCreateButtonLabel.Location = new System.Drawing.Point(331, 200);
             this.charakterCreateButtonLabel.Name = "charakterCreateButtonLabel";
             this.charakterCreateButtonLabel.Size = new System.Drawing.Size(39, 13);
             this.charakterCreateButtonLabel.TabIndex = 1;
@@ -62,7 +71,7 @@ namespace DNDSimulatorInterface
             // 
             this.charakterNameTextBox.BackColor = System.Drawing.Color.White;
             this.charakterNameTextBox.ForeColor = System.Drawing.Color.Black;
-            this.charakterNameTextBox.Location = new System.Drawing.Point(315, 234);
+            this.charakterNameTextBox.Location = new System.Drawing.Point(301, 216);
             this.charakterNameTextBox.Multiline = true;
             this.charakterNameTextBox.Name = "charakterNameTextBox";
             this.charakterNameTextBox.PasswordChar = '*';
@@ -73,23 +82,119 @@ namespace DNDSimulatorInterface
             // 
             this.onlyNumberTextBox.BackColor = System.Drawing.Color.White;
             this.onlyNumberTextBox.ForeColor = System.Drawing.Color.Black;
-            this.onlyNumberTextBox.Location  = new System.Drawing.Point(315, 105);
-            this.onlyNumberTextBox.Name      = "onlyNumberTextBox";
-            this.onlyNumberTextBox.Size      = new System.Drawing.Size(100, 100);
-            this.onlyNumberTextBox.TabIndex  = 3;
+            this.onlyNumberTextBox.Location = new System.Drawing.Point(301, 141);
+            this.onlyNumberTextBox.Name = "onlyNumberTextBox";
+            this.onlyNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.onlyNumberTextBox.TabIndex = 3;
+            // 
+            // CharakterChooseBox
+            // 
+            this.CharakterChooseBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CharakterChooseBox.FormattingEnabled = true;
+            this.CharakterChooseBox.Location = new System.Drawing.Point(435, 173);
+            this.CharakterChooseBox.Name = "CharakterChooseBox";
+            this.CharakterChooseBox.Size = new System.Drawing.Size(121, 21);
+            this.CharakterChooseBox.TabIndex = 4;
+            // 
+            // ActionChooseBox
+            // 
+            this.ActionChooseBox.FormattingEnabled = true;
+            this.ActionChooseBox.Location = new System.Drawing.Point(563, 173);
+            this.ActionChooseBox.Name = "ActionChooseBox";
+            this.ActionChooseBox.Size = new System.Drawing.Size(121, 21);
+            this.ActionChooseBox.TabIndex = 5;
+            // 
+            // CharakterList
+            // 
+            this.CharakterList.BackColor = System.Drawing.Color.Aqua;
+            this.CharakterList.ForeColor = System.Drawing.Color.Black;
+            this.CharakterList.FormattingEnabled = true;
+            this.CharakterList.Location = new System.Drawing.Point(153, 173);
+            this.CharakterList.Name = "CharakterList";
+            this.CharakterList.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.CharakterList.Size = new System.Drawing.Size(120, 95);
+            this.CharakterList.TabIndex = 6;
+            // 
+            // CharakterListButton
+            // 
+            this.CharakterListButton.Location = new System.Drawing.Point(173, 274);
+            this.CharakterListButton.Name = "CharakterListButton";
+            this.CharakterListButton.Size = new System.Drawing.Size(75, 23);
+            this.CharakterListButton.TabIndex = 7;
+            this.CharakterListButton.Text = "Show elements in charakter list";
+            this.CharakterListButton.UseVisualStyleBackColor = true;
+            // 
+            // CharakterSheet
+            // 
+            this.CharakterSheet.FormattingEnabled = true;
+            this.CharakterSheet.Location = new System.Drawing.Point(27, 173);
+            this.CharakterSheet.Name = "CharakterSheet";
+            this.CharakterSheet.Size = new System.Drawing.Size(120, 95);
+            this.CharakterSheet.TabIndex = 8;
+            // 
+            // ShowColumnsButton
+            // 
+            this.ShowColumnsButton.Location = new System.Drawing.Point(145, 303);
+            this.ShowColumnsButton.Name = "ShowColumnsButton";
+            this.ShowColumnsButton.Size = new System.Drawing.Size(150, 23);
+            this.ShowColumnsButton.TabIndex = 9;
+            this.ShowColumnsButton.Text = "Show Columns";
+            this.ShowColumnsButton.UseVisualStyleBackColor = true;
+            // 
+            // CheckedActionList
+            // 
+            this.CheckedActionList.FormattingEnabled = true;
+            this.CheckedActionList.Items.AddRange(new object[] {
+            "Sunday",
+            "Monday",
+            "Tuesday"});
+            this.CheckedActionList.Items.Add("Wednesday", CheckState.Checked);
+            this.CheckedActionList.Items.Add("Thursday", CheckState.Unchecked);
+            this.CheckedActionList.Items.Add("Friday", CheckState.Indeterminate);
+            this.CheckedActionList.Location = new System.Drawing.Point(407, 216);
+            this.CheckedActionList.Name = "CheckedActionList";
+            this.CheckedActionList.Size = new System.Drawing.Size(120, 94);
+            this.CheckedActionList.TabIndex = 10;
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(533, 216);
+            this.CheckButton.Name = "Check";
+            this.CheckButton.Size = new System.Drawing.Size(75, 23);
+            this.CheckButton.TabIndex = 11;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            // 
+            // UnCheck
+            // 
+            this.UnCheckButton.Location = new System.Drawing.Point(533, 245);
+            this.UnCheckButton.Name = "UnCheck";
+            this.UnCheckButton.Size = new System.Drawing.Size(75, 23);
+            this.UnCheckButton.TabIndex = 12;
+            this.UnCheckButton.Text = "UnCheck";
+            this.UnCheckButton.UseVisualStyleBackColor = true;
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(727, 422);
+            this.Controls.Add(this.UnCheckButton);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.CheckedActionList);
+            this.Controls.Add(this.ShowColumnsButton);
+            this.Controls.Add(this.CharakterSheet);
+            this.Controls.Add(this.CharakterListButton);
+            this.Controls.Add(this.CharakterList);
+            this.Controls.Add(this.ActionChooseBox);
+            this.Controls.Add(this.CharakterChooseBox);
             this.Controls.Add(this.onlyNumberTextBox);
             this.Controls.Add(this.charakterNameTextBox);
             this.Controls.Add(this.charakterCreateButtonLabel);
             this.Controls.Add(this.charakterCreateButton);
             this.Name = "MainMenu";
             this.Text = "Main menu";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.MainMenu_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +206,15 @@ namespace DNDSimulatorInterface
         private System.Windows.Forms.Label charakterCreateButtonLabel;
         private System.Windows.Forms.TextBox charakterNameTextBox;
         private System.Windows.Forms.TextBox onlyNumberTextBox;
+        private ComboBox CharakterChooseBox;
+        private ComboBox ActionChooseBox;
+        private ListBox CharakterList;
+        private Button CharakterListButton;
+        private ListBox CharakterSheet;
+        private Button ShowColumnsButton;
+        private CheckedListBox CheckedActionList;
+        private Button CheckButton;
+        private Button UnCheckButton;
     }
 }
 
--- DNDSimulatorInterface/Form1.cs
+++ DNDSimulatorInterface/Form1.cs
@@ -12,12 +12,15 @@ namespace DNDSimulatorInterface
 {
     public partial class MainMenu : Form
     {
+        List<string> fQ = new List<string>();
+        List<string> sQ = new List<string>();
+
         public MainMenu()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void MainMenu_Load(object sender, EventArgs e)
         {
             charakterCreateButtonLabel.Text         = "Leads to charakter creation menu";
             charakterCreateButtonLabel.BorderStyle  = BorderStyle.FixedSingle;
@@ -28,11 +31,24 @@ namespace DNDSimulatorInterface
             charakterNameTextBox.TextChanged += new EventHandler(charakterNameTextBox_TextChanged);
             onlyNumberTextBox.TextChanged += new EventHandler(onlyNumberTextBox_TextChanged);
             onlyNumberTextBox.KeyPress += new KeyPressEventHandler(onlyNumberTextBox_KeyPress);
-            
+            CharakterChooseBox.SelectedIndexChanged += new EventHandler(CharakterChooseBox_SelectedIndexChanged);
+            CharakterList.DoubleClick += new EventHandler(charakterListElement_Click);
+            //CharakterList.SelectedIndexChanged += new EventHandler(CharakterList_SelectedIndexChanged);
+            CharakterListButton.Click += new EventHandler(CharakterListButton_Click_1);
+            CharakterListButton.Click += new EventHandler(CharakterListButton_Click_2);
+            ShowColumnsButton.Click += new EventHandler(CharakterListButton_Click_3);
+            CheckButton.Click += new EventHandler(CheckAllitems);
+            UnCheckButton.Click += new EventHandler(UncheckAllitems);
 
             Controls.Add(charakterCreateButton);
             Controls.Add(charakterNameTextBox);
             Controls.Add(onlyNumberTextBox);
+            Controls.Add(CharakterChooseBox);
+            Controls.Add(CharakterList);
+            Controls.Add(CharakterListButton);
+            Controls.Add(ShowColumnsButton);
+            Controls.Add(CheckButton);
+            Controls.Add(UnCheckButton);
 
             //int i;
             //i = int.Parse(charakterNameTextBox.Text);
@@ -40,6 +56,35 @@ namespace DNDSimulatorInterface
             //f = float.Parse(charakterNameTextBox.Text);
             //double d;
             //d = float.Parse(charakterNameTextBox.Text);
+
+            string var;
+            var = CharakterChooseBox.Text;
+
+            CharakterChooseBox.Items.Add("weekdays");
+            CharakterChooseBox.Items.Add("year");
+            CharakterChooseBox.SelectedItem = var;
+            
+            CharakterList.Items.Add("Monday");
+            CharakterList.Items.Add("Tuesday");
+            CharakterList.Items.Add("Wednesday");
+            CharakterList.Items.Add("Thursday");
+            CharakterList.Items.Add("Friday");
+            CharakterList.Items.Add("Saturday");
+            CharakterList.Items.Add("Sunday");
+            CharakterList.Items.Add("First Quarter");
+            CharakterList.Items.Add("Second Quarter");
+            string item = CharakterList.GetItemText(CharakterList.SelectedItem);
+            
+            fQ.Add("January");
+            fQ.Add("February");
+            fQ.Add("March");
+            sQ.Add("April");
+            sQ.Add("May");
+            sQ.Add("June");
+            List<string> nList = new List<string>();
+            nList.Add("First Quarter");
+            nList.Add("Second Quarter");
+            CharakterList.DataSource = nList;
         }
 
         private void ShowMessage(object sender, EventArgs e)
@@ -74,11 +119,118 @@ namespace DNDSimulatorInterface
                 onlyNumberTextBox.Text = "";
             }
         }
+
         private void onlyNumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
             {
                 e.Handled = true;
+            }
+        }
+
+        private void CharakterChooseBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ActionChooseBox.Items.Remove(ActionChooseBox.SelectedItem);
+            ActionChooseBox.Items.Clear();
+            if (CharakterChooseBox.SelectedItem == "weekdays")
+            {
+                ActionChooseBox.Items.Add("Sunday");
+                ActionChooseBox.Items.Add("Monday");
+                ActionChooseBox.Items.Add("Tuesday");
+            }
+            else if (CharakterChooseBox.SelectedItem == "year")
+            {
+                ActionChooseBox.Items.Add("2012");
+                ActionChooseBox.Items.Add("2013");
+                ActionChooseBox.Items.Add("2014");
+            }
+        }
+
+        private void charakterListElement_Click(object sender, EventArgs e)
+        {
+            foreach (Object obj in CharakterList.SelectedItems)
+            {
+                MessageBox.Show(obj.ToString());
+            }
+        }
+
+        private void CharakterListButton_Click_1(object sender, EventArgs e)
+        {
+            string items = "";
+            foreach (var item in CharakterList.Items)
+            {
+                if (item == null)
+                { }
+                else items += item.ToString() + ", ";
+            }
+            MessageBox.Show(items);
+        }
+
+        private void CharakterListButton_Click_2(object sender, EventArgs e)
+        {
+            List<string> nList = new List<string>();
+            nList.Add("January");
+            nList.Add("February");
+            nList.Add("March");
+            nList.Add("April");
+            CharakterList.DataSource = nList;
+        }
+
+        //private void CharakterList_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    if (CharakterList.SelectedItem==null)
+        //    { }
+        //    else MessageBox.Show(CharakterList.SelectedItem.ToString());
+        //}
+
+        //private void CharakterList_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    if (CharakterList.SelectedIndex == 0)
+        //    {
+        //        CharakterSheet.DataSource = null;
+        //        CharakterSheet.DataSource = fQ;
+        //    }
+        //    else if (CharakterList.SelectedIndex == 1)
+        //    {
+        //        CharakterSheet.DataSource = null;
+        //        CharakterSheet.DataSource = sQ;
+        //    }
+        //}
+
+        private void CharakterListButton_Click_3(object sender, EventArgs e)
+        {
+            CharakterList.DataSource = null;
+            CharakterList.HorizontalScrollbar = true;
+            CharakterList.MultiColumn = true;
+            CharakterList.ScrollAlwaysVisible = true;
+            CharakterList.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+        }
+
+        private void UncheckAllitems(object sender, EventArgs e)
+        {
+            for (int i = 0; i < CheckedActionList.Items.Count; i++)
+            {
+                CheckedActionList.SetItemChecked(i, false);
+            }
+        }
+
+        private void CheckAllitems(object sender, EventArgs e)
+        {
+            for (int i = 0; i < CheckedActionList.Items.Count; i++)
+            {
+                CheckedActionList.SetItemChecked(i, true);
             }
         }
     }
